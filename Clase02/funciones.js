c7f1465300f1d4709c450369994d14b236091da2
maxiVidarte <maxiavidarte@gmail.com>
2017-08-25 21:10:28 -0300

    clase02

---
+++ Clase02/funciones.js
@@ -0,0 +1,15 @@
+     
+       function validar(){
+            var nombre = $("txtUsr").value;   
+            var contraseña = $("txtPass").value;
+        if(nombre  == "maxi"){
+           alert("ok");
+        }
+        else
+        alert("error");
+        }
+        
+        function $(identificador)
+        {
+            return document.getElementById(identificador);
+        }
