dbf343d7e2034efd7b9c07fffa09b3e760cd5ea0
maxiVidarte <maxiavidarte@gmail.com>
2017-09-08 22:03:46 -0300

    falta el estilo de la validacion

--- Clase04/script.js
+++ Clase04/script.js
@@ -4,8 +4,13 @@
          
             if(xmlHttp.readyState === 4){
                 if(xmlHttp.status === 200){
-         
-                    console.log(xmlHttp.response);
+                    if(xmlHttp.response == "true"){
+                        
+                        alert("Usuario y contraseña correcta");
+                    }
+                    else 
+                    {   alert("Usuario y contraseña incorrecta");}
+                   // console.log(xmlHttp.response);
                 }
             }
         }
@@ -15,14 +20,33 @@
             xmlHttp.open("GET",url,true);
             xmlHttp.send();
         }
-        function valida(){
+       /* function valida(){
             var usuario = document.getElementById("usr").value;
             var contraseña = document.getElementById("pass").value;
+            var usr = "";
             if(usuario== "" || contraseña == "")
                 var usr = "http://localhost:3000/loginUsuario";
             else
                 var usr = "http://localhost:3000/loginUsuario?usr="+usuario+"&pass="+contraseña;
                 return usr;
+        }*/
+        function valida(){
+            var usuario = document.getElementById("usr").value;
+            var contraseña = document.getElementById("pass").value;
+            var usr = "";
+            if(usuario== "" || contraseña == "")
+                {}
+            else
+                usr = "usr="+usuario+"&pass="+contraseña;
+                return usr;
+        }
+        function peticionPost(){
+            xmlHttp.onreadystatechange = callback; 
+            var url = valida();       
+            xmlHttp.open("POST","http://localhost:3000/loginUsuario",true);
+            xmlHttp.setRequestHeader("content-type","application/x-www-form-urlencoded");
+            xmlHttp.send(url);
+            
         }
         /*function peticionGet(){
             xmlHttp.onreadystatechange = callback;        
