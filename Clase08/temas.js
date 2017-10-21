a51447dd46e7f27c46591906215f1f690486e422
maxiVidarte <maxiavidarte@gmail.com>
2017-10-20 22:00:09 -0300

    carga del tp

--- Clase08/temas.js
+++ Clase08/temas.js
@@ -11,4 +11,6 @@ num.map(num => num*num);//va a operar un numero que se multiplica y lo retorna. 
 num.filter(num=>num>3);//esto devuelve los numeros mayores a 3
 //reduce devuelve un resultado unico de cada valor o sea un solo dato... 
 num.reduce(function (total,num){return total+=num;},0);// el 0 es otro parametro del reduce, que seria como se inicializa el acumulador
-valor de variable que va acumulando "total", y el item "num" 
+valor de variable que va acumulando "total", y el item "num"
+//2)bootstrap es un conjunto de estilos
+//class= "col-xs-12 col-sm-8  col-md-9 col-lg-9"
